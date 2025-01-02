29c346ac6557bdd5087971811af6e47047b419fd
Seth Landry <waymakerijn@outlook.com>
2025-01-01 19:08:29 -0600

    Add placeholders and instructions for ClickupAPIWrapper initialization
    * Add placeholders for `oauth_client_id`, `oauth_client_secret`, and `redirect_uri`
    * Add instruction to click the link and connect workspace

--- sandbox.js
+++ sandbox.js
@@ -5,3 +5,6 @@ redirect_uri = "https://google.com"
 
 print("Click this link, select your workspace, click `Connect Workspace`")
 print(ClickupAPIWrapper.get_access_code_url(oauth_client_id, redirect_uri))
+
+# Click this link, select your workspace, click `Connect Workspace`
+# https://app.clickup.com/api?client_id=ABC...&redirect_uri=https://google.com
