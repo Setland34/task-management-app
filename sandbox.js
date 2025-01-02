79d908a8069f7357a007ae886f4f3bf227f06987
Seth Landry <waymakerijn@outlook.com>
2025-01-01 19:12:29 -0600

    * Add code to initialize ClickupAPIWrapper and ClickupToolkit
    * Add placeholders for `oauth_client_id`, `oauth_client_secret`, and `redirect_uri`
    * Add instruction to click the link and connect workspace
    * Add code to set `code` variable to "THISISMYCODERIGHTHERE"
    * Add code to set `access_token` using `ClickupAPIWrapper.get_access_token`

--- sandbox.js
+++ sandbox.js
@@ -13,3 +13,10 @@ code = "THISISMYCODERIGHTHERE"
 access_token = ClickupAPIWrapper.get_access_token(
     oauth_client_id, oauth_client_secret, code
 )
+
+# Init toolkit
+clickup_api_wrapper = ClickupAPIWrapper(access_token=access_token)
+toolkit = ClickupToolkit.from_clickup_api_wrapper(clickup_api_wrapper)
+print(
+    f"Found team_id: {clickup_api_wrapper.team_id}.\nMost request require the team id, so we store it for you in the toolkit, we assume the first team in your list is the one you want. \nNote: If you know this is the wrong ID, you can pass it at initialization."
+)
